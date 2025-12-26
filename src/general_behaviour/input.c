e4e91cf7d59439cdda1923dbd88208710254b559
AbbasCherri <abbasbcherri@gmail.com>
2025-12-26 09:08:31 +0000

    Add buffer clear

--- src/general_behaviour/input.c
+++ src/general_behaviour/input.c
@@ -8,22 +8,32 @@ void print_buffer();
 char buffer[BUFFER_SIZE];
 
 int take_input() {
-    printf("msh>");
-    
-    if (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
-        buffer[strcspn(buffer, "\n")] = '\0';
-    } else {
-        printf("ERROR TAKING INPUT\n");
-        return -1;
-    }
+    while (1) {
+        printf("msh>");
+        
+        if (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
+            buffer[strcspn(buffer, "\n")] = '\0';
+        } else {
+            printf("ERROR TAKING INPUT\n");
+            return -1;
+        }
 
-    print_buffer();
+        print_buffer();
+    }
     return 0;
 }
 
 void print_buffer() {
-    for (int i = 0; i < BUFFER_SIZE && buffer[i] != '/0'; i++) {
+    int i;
+    for (i = 0; i < BUFFER_SIZE && buffer[i] != '\0'; i++) {
         printf("%c", buffer[i]);
     }
     printf("\n");
+
+}
+
+void clear_buffer() {
+    for (int i = 0; i < BUFFER_SIZE && buffer[i] != '\0'; i++) {
+        buffer[i] = '\0';
+    }
 }
